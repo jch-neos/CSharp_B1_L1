cbbcbb1a45649805c0ba8d317e0de58d46d97f86
Julien Cheritat <julien.cheritat@neos-sdi.com>
2023-01-07 11:17:59 +0100

    ex 3 extras

--- Program.cs
+++ Program.cs
@@ -1,8 +1,21 @@
-﻿// exercise 3: Manage exceptions 
-Random r=new Random(2321);
-async Task RunLongProcessExceptions(int milliseconds, CancellationToken ct=default){
-    if(r.NextDouble()>0.9) {
+﻿// exercise 3 : Manage exceptions so that there is no error when the timeout is exceeded
+// exercise 3.1 : detect when cancellation was requested
+const int timeout = 300;
+using var cts = new CancellationTokenSource(timeout);
+Random r = new Random();
+
+async Task RunLongProcessExceptions(int num, int milliseconds, CancellationToken ct = default)
+{
+    if (r.NextDouble() > 0.999)
+    {
         throw new Exception("Random Exception");
     }
+    Console.WriteLine($"OK : {num}");
     await Task.Delay(milliseconds, ct);
 }
+
+for (int i = 0; i < 100; i++)
+{
+    await RunLongProcessExceptions(i, 20, cts.Token);
+}
+Console.WriteLine("OK");
