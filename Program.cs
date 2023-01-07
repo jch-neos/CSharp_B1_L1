16d4d83aa2777902fb78de543011d09fbbbab825
Julien Cheritat <julien.cheritat@neos-sdi.com>
2023-01-07 11:32:02 +0100

    ex1.1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,21 @@
-﻿// exercise 1 : Make the following code run to completion
+﻿// exercise 1.1 : why is the code slow to run ? can you correct it ?
+using System.Diagnostics;
 
-Task write = new Task(()=>Console.WriteLine("Doing some work"));
-await write;
+const int count = 100;
+Task[] tasks = new Task[count];
+var start = Stopwatch.GetTimestamp();
+for (int i = 0; i < count; i++)
+{
+    tasks[i] = Run();
+}
+for (int i = 0; i < count; i++)
+{
+    await tasks[i];
+}
+var end = Stopwatch.GetElapsedTime(start);
+Console.WriteLine($"Elapsed : {end.TotalMilliseconds}");
 
+async Task<int> Run() {
+    Thread.Sleep(100);
+    return await Task.FromResult(1);
+}
