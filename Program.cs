8a3b04eb9544ae49508360a76c75537f67e51af0
Julien Cheritat <julien.cheritat@neos-sdi.com>
2023-02-01 12:13:35 +0100

    WaitAll

--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
-﻿// exercise 1 : Make the following code run to completion
+﻿using System.Diagnostics;
 
-Task write = new Task(()=>Console.WriteLine("Doing some work"));
-await write;
+var ts = Stopwatch.GetTimestamp();
 
+Stopwatch.GetElapsedTime(ts);
+
+var arr = Enumerable.Range(1, 20).Select(Test).ToArray();
+var t = Task.WhenAny(arr);
+var finished = await t;
+Console.WriteLine($"task #{Array.IndexOf(arr, finished)} : {await finished}");
+
+Console.WriteLine(Stopwatch.GetElapsedTime(ts));
+
+async Task<string> Test(int x)
+{
+    var delay = Random.Shared.Next(2000) * 100;
+    await Task.Delay(delay);
+    return $"task delay {delay}";
+}
