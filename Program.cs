7524a77a36ae29ba8a7af6babf53bf52ef532f27
Julien Cheritat <julien.cheritat@neos-sdi.com>
2023-02-01 11:54:17 +0100

    WhenAll

--- Program.cs
+++ Program.cs
@@ -1,5 +1,27 @@
-﻿// exercise 1 : Make the following code run to completion
+﻿using System.Diagnostics;
 
-Task write = new Task(()=>Console.WriteLine("Doing some work"));
-await write;
+var ts = Stopwatch.GetTimestamp();
 
+Stopwatch.GetElapsedTime(ts);
+
+var arr = Enumerable.Range(1, 200).Select(Test);
+var t = Task.WhenAll(arr);
+try {
+  await t;
+} catch (Exception e) {
+  Console.WriteLine(e);
+}
+
+
+Console.WriteLine(Stopwatch.GetElapsedTime(ts));
+
+async Task Test(int x) {
+  await Task.Delay(1000);
+  if (x == 199) {
+  }
+  if (x > 190) {
+    throw new OperationCanceledException();
+    throw new Exception($"{x} > 190");
+  }
+  await Console.Out.WriteLineAsync($"Test  : {System.Threading.Thread.CurrentThread.ManagedThreadId}");
+}
