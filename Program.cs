e99596add8bd329b716ae7e860efbe55afa779b9
Julien Cheritat <julien.cheritat@neos-sdi.com>
2023-02-01 11:50:04 +0100

    Cancelatio Token Source

--- Program.cs
+++ Program.cs
@@ -1,5 +1,35 @@
-﻿// exercise 1 : Make the following code run to completion
+﻿
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
-Task write = new Task(()=>Console.WriteLine("Doing some work"));
-await write;
+public class C {
+    public static async ValueTask<int> M(CancellationToken ct) {
+        await Task.Delay(100, ct);        
+        await Task.Delay(100, ct);
+        if(ct.IsCancellationRequested) return await ValueTask.FromCanceled<int>(ct);
+        return 2;
+    }
+    public static async Task Main() {
 
+        var cts = new CancellationTokenSource();
+
+        cts.CancelAfter(10);
+
+        var t = M(cts.Token);
+        await Task.Delay(100);        
+        var r = t.GetAwaiter();
+
+        Console.WriteLine((
+            t.IsCanceled,
+            t.IsCompleted,
+            t.IsFaulted,
+            t.IsCompletedSuccessfully
+        ));
+        Console.WriteLine(r.IsCompleted);
+        Console.WriteLine(r.GetResult());
+
+    }
+    
+}
